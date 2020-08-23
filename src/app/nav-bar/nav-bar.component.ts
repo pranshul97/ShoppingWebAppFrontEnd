2493f76e8398b53b944ac61a65d71f3802d6f8e3
Pranshul Tiwari <pranshul.tiwari97@gmail.com>
2020-08-23 22:55:53 +0530

    Changes in Navbar and display-product component.

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { DisplayProductComponent } from '../display-product/display-product.component';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -9,11 +9,12 @@ import { Router } from '@angular/router';
 export class NavBarComponent {
 
   constructor(private router: Router) { }
-
+  
   productId: any;
   seeproduct(productId: number){
     this.productId=productId;
     sessionStorage.setItem("productIdToBeDisplayed",this.productId);
     this.router.navigate(['app-display-product']);
+    
   }
 }
