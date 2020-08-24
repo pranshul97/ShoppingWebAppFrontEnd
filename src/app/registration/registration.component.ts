cc5c8c8ec49cae822957345114ae4a85baf93063
Bhavya <BHAVYA.Kumar@lntinfotech.com>
2020-08-24 18:28:04 +0530

    reatailer registration and login

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RetailerService } from "../retailer.service";
+import { Retailer } from "./retailer";
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  retailer: Retailer = new Retailer();
+  message: any;
+
+  constructor(private service: RetailerService) { }
 
   ngOnInit(): void {
+  }
+
+  addRetailer(){
+    this.service.addRetailer(this.retailer).subscribe(
+      data => {
+        alert(JSON.stringify(this.retailer));
+      }
+    )
+    this.message = "Retailer registered successfully";
   }
 
 }
