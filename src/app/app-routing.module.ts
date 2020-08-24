5efb6f31cdd5f2f794f20534796a120aba875864
Pranshul Tiwari <pranshul.tiwari97@gmail.com>
2020-08-24 15:07:30 +0530

    Linked compare module with Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { DisplayProductComponent } from './display-product/display-product.component';
+import { CompareComponent } from './compare/compare.component';
 
 const routes: Routes = [
   {path: 'app-register',component: RegistrationComponent},
   {path: 'app-login' , component: LoginComponent},
-  {path: 'app-display-product', component: DisplayProductComponent}
+  {path: 'app-display-product', component: DisplayProductComponent},
+  {path: 'app-compare', component: CompareComponent}
 ];
 
 @NgModule({
