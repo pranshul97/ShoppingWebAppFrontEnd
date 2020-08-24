a87b4e0a87f3aa652e1afd5fcc4b0ef4345687a1
Pranshul Tiwari <pranshul.tiwari97@gmail.com>
2020-08-24 19:29:33 +0530

    Integrated user registration and login component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { DisplayProductComponent } from './display-product/display-product.component';
 import { CompareComponent } from './compare/compare.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 const routes: Routes = [
   {path: 'app-register',component: RegistrationComponent},
   {path: 'app-login' , component: LoginComponent},
   {path: 'app-display-product', component: DisplayProductComponent},
-  {path: 'app-compare', component: CompareComponent}
+  {path: 'app-compare', component: CompareComponent},
+  { path: 'app-user-registration', component: UserRegistrationComponent},
+  {path: 'app-user-login', component: UserLoginComponent}
 ];
 
 @NgModule({
