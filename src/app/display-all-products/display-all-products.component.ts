d714ec1a613ad913c676039f15eb470c3a3c8007
Pranshul Tiwari <pranshul.tiwari97@gmail.com>
2020-08-26 18:38:46 +0530

    Categories and brand based filtering completed.

--- src/app/display-all-products/display-all-products.component.ts
+++ src/app/display-all-products/display-all-products.component.ts
@@ -11,35 +11,60 @@ export class DisplayAllProductsComponent implements OnInit {
   constructor(private route: ActivatedRoute, private service: SearchProductsService, private router: Router) { }
   compareArr: number[]=[];
   data: any;
+  allCategories: any[]=[];
+  categories: any[]=[];
   categoriesSelected: any[]=[];
   brandsSelected: any[]=[];
+  allBrands:any[]=[];
+  brands: any[]=[];
   price: PriceFilter=new PriceFilter();
   min: number;
   max: number;
   ngOnInit(): void {
-    this.min=0;
-    this.max=Number.MAX_VALUE;
+    
     this.route.params.subscribe(
       params=>{
         const value= params['value'];
+        this.min=0;
+        this.max=Number.MAX_VALUE;
+        this.allCategories=[];
+        this.categories=[];
+        this.categoriesSelected=[];
+        this.brandsSelected=[];
+        this.allBrands=[];
+        this.brands=[];
         //alert(value);
         this.service.searchByName(value).subscribe(data=>{
-          this.data=data;
+          this.data=data
           alert(JSON.stringify(data));
-          /*if(this.data.status=="SUCCESS"){
-            alert("product Found");
+          for(let item of this.data.list){
+            if(!this.allCategories.includes(item.categoryName)){
+              this.allCategories.push(item.categoryName);
+              this.categoriesSelected.push(item.categoryName);
+            }
+            if(!this.allBrands.includes(item.brandName)){
+              this.allBrands.push(item.brandName);
+              this.brandsSelected.push(item.brandName);
+            }
           }
-          else{
-            alert("Product not found");
-          }*/
-          /*for(let item of this.data.list){
-            //alert(item.name+", "+item.description);
-          }*/
         })
       }
+      
     )
+    //this.getCategories(JSON.stringify(this.data));
   }
 
+  getCategories(data: any){
+    alert(this.data.json());
+    /*
+    for(let item of this.data.list){
+      alert("Yo")
+      if(this.categories.has(item.categoryName)){
+        this.categories.set(item.categoryName,1)
+      }
+    }
+    alert(this.categories);*/
+  }
   addToCompare(id: number){
     alert(id);
     this.compareArr.push(id);
@@ -51,13 +76,64 @@ export class DisplayAllProductsComponent implements OnInit {
     this.router.navigate(['/app-compare']);
   }
 
-  check(dat: any){
-    alert(dat);
+  check(event,dat: any){
+    if(event.target.checked){
+      this.categories.push(dat);
+      //alert("hi");
+    }
+    else{
+      for(let i=0;i<this.categories.length;i++){
+        if(this.categories[i]==dat){
+            this.categories.splice(i,1);
+        }
+      }
+    }
+    alert(this.categories);
   }
 
+  check1(event,dat: any){
+    if(event.target.checked){
+      this.brands.push(dat);
+      //alert("hi");
+    }
+    else{
+      for(let i=0;i<this.brands.length;i++){
+        if(this.brands[i]==dat){
+            this.brands.splice(i,1);
+        }
+      }
+    }
+    alert(this.brands);
+  }
+
+
+  applyCategory(){
+    if(this.categories.length>0){
+      this.categoriesSelected=[];
+      for(let item of this.categories){
+        this.categoriesSelected.push(item);
+      }
+      //this.categories=[];
+    }
+    else{
+      this.categoriesSelected=this.allCategories;
+    }
+  }
   applyPrice(){
     this.min=this.price.minval;
     this.max=this.price.maxval;
+  }
+
+  applyBrands(){
+    if(this.brands.length>0){
+      this.brandsSelected=[];
+      for(let item of this.brands){
+        this.brandsSelected.push(item);
+      }
+    }
+    else{
+      this.brandsSelected=this.allBrands;
+    }
   }
 }
 
