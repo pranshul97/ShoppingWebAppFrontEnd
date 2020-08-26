5ec5c4c8d8039051f23e56da9ed887e2c4c6cbb3
Pranshul Tiwari <pranshul.tiwari97@gmail.com>
2020-08-26 11:46:10 +0530

    Added Categories in Navbar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DisplayProductComponent } from '../display-product/display-product.component';
+import { RetailerService } from '../retailer.service';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent {
-
-  constructor(private router: Router) { }
-  
+export class NavBarComponent implements OnInit {
+  categoryData: any;
+  constructor(private router: Router,private retailerService: RetailerService) {
+   }
+  ngOnInit(): void{
+    this.retailerService.fetchCategory().subscribe(data =>{
+      this.categoryData=data;
+      alert(JSON.stringify(this.categoryData));
+    })
+  }
   productId: any;
   seeproduct(productId: number){
     this.productId=productId;
